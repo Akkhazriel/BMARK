f3e8e1ab3299cf2efdf7ac37c7dcd48c03f1d6a8
Akkhazriel <akkhazriel@gmail.com>
2023-08-15 18:35:18 +0300

    First update. HTML-CSS-JS

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import menuActive from "./modules/menu-button";
+import activeLink from "./modules/menu-inputs";
+import { autoCompleteJs } from "./modules/menu-inputs";
+import { select } from "./modules/menu-inputs";
+select();
