61d6e28bf60394cdecee4ee6da3a8ca5826e648b
Akkhazriel <akkhazriel@gmail.com>
2023-08-16 15:38:17 +0300

    section properties + js slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,19 @@ import menuActive from "./modules/menu-button";
 import activeLink from "./modules/menu-inputs";
 import { autoCompleteJs } from "./modules/menu-inputs";
 import { select } from "./modules/menu-inputs";
+import Swiper from 'swiper/bundle';
+
 select();
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    
+    keyboard: {
+        enable: true,
+    },
+
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    },
+});
